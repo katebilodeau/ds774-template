d59079acab6cbf528dcd4050ef1a5133a9402563
Kate Bilodeau <kate.bilodeau@unh.edu>
2022-03-21 19:31:10 -0400

    redirect

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def contact():
         result = contact_form(fname, lname, eaddress, message)
 
         if result:
-            return render_template('contact.html', message='Thank you for your submission')
+            return redirect('/issueslog')
         else:
             return render_template('contact.html', message='Error with submission')
     else:
